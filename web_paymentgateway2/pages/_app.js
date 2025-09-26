ac9c1106e0799ce2da15038954d29d2a48f57a89
prissjasmine05 <priscillajk05@gmail.com>
2025-09-26 21:35:57 +0700

    _app.js update

--- web_paymentgateway2/pages/_app.js
+++ web_paymentgateway2/pages/_app.js
@@ -1,9 +1,10 @@
-// pages/_app.js
 import '../styles/globals.css';
 import { CartProvider } from '../context/CartContext';
 
+// Ini adalah komponen "kulit" utama untuk semua halaman
 function MyApp({ Component, pageProps }) {
   return (
+    // CartProvider membungkus semua halaman agar keranjang bisa diakses di mana saja
     <CartProvider>
       <Component {...pageProps} />
     </CartProvider>
